ef472fb87822ec73e4bbe01bb56298d919a9116a
MoldenMilk <christianpyb@gmail.com>
2021-10-25 17:33:15 +0700

    Push Yap

--- app/src/main/java/com/example/pchop/CheckOut.java
+++ app/src/main/java/com/example/pchop/CheckOut.java
@@ -2,13 +2,26 @@ package com.example.pchop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class CheckOut extends AppCompatActivity {
+    private MaterialButton btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CheckOut.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
